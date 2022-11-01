ae79d7c9973692f0982d38d738d0ed59bc2a5c98
B3ns2005 <60721397+bkrstrawson@users.noreply.github.com>
2022-11-01 12:17:05 -0600

    rfg

--- flow free/sketch.js
+++ flow free/sketch.js
@@ -8,9 +8,11 @@
 let grid = [];
 let gridSize = 5;
 let cellsize;
-let colorState;
+let colorState = " ";
 let xpos;
 let ypos;
+let xposPast;
+let yposPast;
 
 
 
@@ -31,10 +33,15 @@ function draw() {
   background(220);
   cellsize = height/gridSize;
   displayGrid(grid);
-  console.log(grid);
+ 
+ 
   drawCircles(grid);
-  xpos = Math.floor(mouseX/cellsize);
-  ypos = Math.floor(mouseY/cellsize);
+  
+  xpos = Math.floor ((mouseX-width/4)/cellsize)  ;
+  ypos = Math.floor (mouseY/cellsize)  ;
+  drawLines();
+  //console.log(xpos) ;
+  // console.log(ypos) ;
 }
 
 function createGrid(){
@@ -70,7 +77,6 @@ function drawCircles(grid){
         fill ("blue");
         circle(x*cellsize+ width/4 +cellsize/2,y*cellsize+cellsize/2,cellsize/2);
       }
-    
       if(grid[y][x] === "R"){
         fill ("red");
         circle(x*cellsize+ width/4 +cellsize/2,y*cellsize+cellsize/2,cellsize/2);
@@ -79,24 +85,63 @@ function drawCircles(grid){
         fill ("yellow");
         circle(x*cellsize+ width/4 +cellsize/2,y*cellsize+cellsize/2,cellsize/2);
       }
+      if(grid[y][x] === "g"|| grid[y][x] === "endg"){
+        fill ("green");
+        circle(x*cellsize+ width/4 +cellsize/2,y*cellsize+cellsize/2,cellsize/3);
+      }
+      if(grid[y][x] === "b"||grid[y][x] === "endb"){
+        fill ("blue");
+        circle(x*cellsize+ width/4 +cellsize/2,y*cellsize+cellsize/2,cellsize/3);
+      }
+      if(grid[y][x] === "r"|| grid[y][x] === "endr"){
+        fill ("red");
+        circle(x*cellsize+ width/4 +cellsize/2,y*cellsize+cellsize/2,cellsize/3);
+      }
+      if(grid[y][x] === "y"|| grid[y][x] === "endy"){
+        fill ("yellow");
+        circle(x*cellsize+ width/4 +cellsize/2,y*cellsize+cellsize/2,cellsize/3);
+      }
     }
   }
 }
 
 function drawLines(){
-  if (grid[ypos][xpos]==="G"){
-    colorState = "green"
+  if (xpos >= 0 && xpos < gridSize && ypos >=0 && ypos < gridSize) {
+    if (grid[ypos][xpos]==="G" || grid[ypos][xpos]==="g" ){
+      colorState = "green";
+    }
+    else if (grid[ypos][xpos]==="B"|| grid[ypos][xpos]==="b"){
+      colorState = "blue";
+    }
+    else if (grid[ypos][xpos]==="Y" || grid[ypos][xpos]==="y"){
+      colorState = "yellow";
+    }
+    else if (grid[ypos][xpos]==="R"|| grid[ypos][xpos]==="r" ){
+      colorState = "red";
+    }
+    else {
+      colorState = " "; 
+    }
   }
-  if (grid[ypos][xpos]==="B"){
-    colorState = "blue"
-  }
-  if (grid[ypos][xpos]==="Y"){
-    colorState = "yellow"
-  }
-  if (grid[ypos][xpos]==="R"){
-    colorState = "red"
-  }
-  while(mouseIsPressed){
+  console.log(colorState);
+  //console.log(xpos)
+  // console.log(ypos)
+  if(mouseIsPressed){
     
+    if (xpos === xposPast +1 || xpos === xposPast -1 || ypos === yposPast +1 ||ypos ===yposPast -1){
+      if ((grid [yposPast][xposPast] === "endr" || grid [yposPast][xposPast] === "R") && grid[ypos][xpos]===0){
+        if (grid [yposPast][xposPast] !== "R") {
+          grid [yposPast][xposPast] = "r";
+        }
+        if (grid[ypos][xpos] === "R"){
+          console.log ("you win");
+        }
+        else {
+          grid[ypos][xpos] = "endr";
+        }
+      }
+    }
   }
+  xposPast = xpos;
+  yposPast = ypos;
 }
