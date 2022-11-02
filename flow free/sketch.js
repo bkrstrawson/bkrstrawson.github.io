f3ebb27f4e2809ddc5a69da0e8f0e0ebd4d7e12a
B3ns2005 <60721397+bkrstrawson@users.noreply.github.com>
2022-11-02 12:17:05 -0600

    did stuff

--- flow free/sketch.js
+++ flow free/sketch.js
@@ -13,16 +13,20 @@ let xpos;
 let ypos;
 let xposPast;
 let yposPast;
+let rDone = false;
+let bDone = false;
+let gDone = false;
+let yDone = false;
 
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   createGrid();
-  grid[0][0] = "R";
+  grid[0][0] = "G";
   grid[4][0] = "R";
-  grid[3][1] = "G";
-  grid[4][2] = "G";
+  grid[3][1] = "R";
+  grid[4][4] = "G";
   grid[1][4] = "Y";
   grid[0][3] = "Y";
   grid[4][3] = "B";
@@ -31,19 +35,22 @@ function setup() {
 
 function draw() {
   background(220);
-  cellsize = height/gridSize;
+  drawStuff();
   displayGrid(grid);
- 
- 
   drawCircles(grid);
-  
-  xpos = Math.floor ((mouseX-width/4)/cellsize)  ;
-  ypos = Math.floor (mouseY/cellsize)  ;
+  checkcollisons();
   drawLines();
-  //console.log(xpos) ;
-  // console.log(ypos) ;
+  checkWin();
 }
-
+function drawStuff(){//just for uday
+  if (mouseX < width && mouseX>0){
+    xpos = Math.floor ((mouseX-width/4)/cellsize); 
+  }
+  if (mouseY < height && mouseY>0){
+    ypos = Math.floor (mouseY/cellsize);
+  }
+  cellsize = height/gridSize;
+}
 function createGrid(){
   for (let i = 0; i < gridSize; i ++){
     grid.push([]);
@@ -123,25 +130,125 @@ function drawLines(){
       colorState = " "; 
     }
   }
-  console.log(colorState);
-  //console.log(xpos)
-  // console.log(ypos)
+  //console.log(colorState);
+  console.log(xpos)
+   console.log(ypos)
   if(mouseIsPressed){
     
     if (xpos === xposPast +1 || xpos === xposPast -1 || ypos === yposPast +1 ||ypos ===yposPast -1){
-      if ((grid [yposPast][xposPast] === "endr" || grid [yposPast][xposPast] === "R") && grid[ypos][xpos]===0){
+      if ((grid [yposPast][xposPast] === "endr" || grid [yposPast][xposPast] === "R") && (grid[ypos][xpos]===0 || grid[ypos][xpos]==="R")&&!rDone){
         if (grid [yposPast][xposPast] !== "R") {
           grid [yposPast][xposPast] = "r";
         }
         if (grid[ypos][xpos] === "R"){
-          console.log ("you win");
+          rDone = true;
         }
         else {
           grid[ypos][xpos] = "endr";
+        }
+      }
+      if ((grid [yposPast][xposPast] === "endb" || grid [yposPast][xposPast] === "B") && (grid[ypos][xpos]===0 || grid[ypos][xpos]==="B")&&!bDone){
+        if (grid [yposPast][xposPast] !== "B") {
+          grid [yposPast][xposPast] = "b";
+        }
+        if (grid[ypos][xpos] === "B"){
+          bDone = true;
+        }
+        else {
+          grid[ypos][xpos] = "endb";
+        }
+      }
+      if ((grid [yposPast][xposPast] === "endy" || grid [yposPast][xposPast] === "Y") && (grid[ypos][xpos]===0 || grid[ypos][xpos]==="Y")&&!yDone){
+        if (grid [yposPast][xposPast] !== "Y") {
+          grid [yposPast][xposPast] = "y";
+        }
+        if (grid[ypos][xpos] === "Y"){
+          yDone = true;
+        }
+        else {
+          grid[ypos][xpos] = "endy";
+        }
+      }
+      if ((grid [yposPast][xposPast] === "endg" || grid [yposPast][xposPast] === "G") && (grid[ypos][xpos]===0 || grid[ypos][xpos]==="G")&& !gDone){
+        if (grid [yposPast][xposPast] !== "G") {
+          grid [yposPast][xposPast] = "g";
+        }
+        if (grid[ypos][xpos] === "G"){
+          gDone = true;
+        }
+        else {
+          grid[ypos][xpos] = "endg";
         }
       }
     }
   }
   xposPast = xpos;
   yposPast = ypos;
+}
+
+function checkcollisons(){
+  if (xpos === xposPast +1 || xpos === xposPast -1 || ypos === yposPast +1 ||ypos ===yposPast -1){
+    if (grid[ypos][xpos]  !==0){
+      if (grid[ypos][xpos] ==="r"||grid[ypos][xpos] ==="endr"){
+        //if (colorstate ==="r" && grid[yposPast][xposPast] ===
+        deleteLine("r");
+      }
+      if (grid[ypos][xpos] ==="g" ||grid[ypos][xpos] ==="endg"){
+        deleteLine("g");
+      }
+      if (grid[ypos][xpos] ==="y"||grid[ypos][xpos] ==="endy"){
+        deleteLine("y");
+      }
+      if (grid[ypos][xpos] ==="b"||grid[ypos][xpos] ==="endb"){
+        deleteLine("b");
+      }
+    }
+  }
+}
+//&& grid[ypos][xpos]  !=="G" && grid[ypos][xpos]  !=="Y" && grid[ypos][xpos]  !=="R" && grid[ypos][xpos]  !=="B"){
+
+function deleteLine(cDel){
+  if(mouseIsPressed && (xposPast !==0 || yposPast !== 0)){
+    if(cDel === "r"){
+      rDone=false;
+    }
+    if(cDel === "y"){
+      yDone=false;
+    }
+    if(cDel === "b"){
+      bDone=false;
+    }
+    if(cDel === "g"){
+      gDone=false;
+    }
+    for (let y=0; y<gridSize; y++) {
+      for (let x=0; x<gridSize; x++) {
+        if (grid[y][x] === cDel || grid[y][x] === "end" + cDel){
+          grid[y][x] =0; 
+
+        }
+      }
+    }
+  }
+}
+
+function checkWin(){
+  if (rDone && bDone && gDone && yDone){
+    console.log("you win");
+  }
+}
+
+function mousePressed(){
+  if (grid[ypos][xpos]==="G"){
+    deleteLine("g");
+  }
+  if (grid[ypos][xpos]==="R"){
+    deleteLine("r");
+  }
+  if (grid[ypos][xpos]==="B"){
+    deleteLine("b");
+  }
+  if (grid[ypos][xpos]==="Y"){
+    deleteLine("y");
+  }
 }
