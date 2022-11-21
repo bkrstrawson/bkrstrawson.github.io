48deec4c2ab60e5870a971dff8e97a5ee1bdae7d
B3ns2005 <60721397+bkrstrawson@users.noreply.github.com>
2022-11-21 11:11:17 -0600

    ju

--- gun oop/sketch.js
+++ gun oop/sketch.js
@@ -6,6 +6,8 @@
 // - describe what you did to take this project "above and beyond"
 let guy;
 let bulletsAR = [];
+let flash = 0
+let flashimg;
 
 class bullet{
   constructor(x,y){
@@ -14,19 +16,22 @@ class bullet{
     this.dx = 5;
   }
   move(){
-    this.x -= this.dx;
+    this.x += this.dx;
   }
   display(){
-    fill("black");
-    circle(this.x,this.y,20);
+    fill("grey");
+    circle(this.x+40,this.y,20);
+    fill("yellow")
+    rect(this.x,this.y-10,40,20)
   }
   delete(){
-    return this.x < 0;
+    return this.x > width;
   }
 }
 
 function preload(){
   guy = loadImage("guy w gun.png");
+  flashimg = loadImage("flash.png")
 }
 
 function setup() {
@@ -35,7 +40,11 @@ function setup() {
 
 function draw() {
   background(220);
-  image(guy,width-guy.width,0);
+  image(guy,0,0);
+  if (flash > 0){
+    image(flashimg,690,125)
+    flash --
+      }
   for (let i = bulletsAR.length-1; i > 0; i --){
     bulletsAR[i].display();
     bulletsAR[i].move();
@@ -43,9 +52,15 @@ function draw() {
       bulletsAR.splice(i, 1);
     }
   }
+  fill("white")
+rect(1151,255,55,20)
+fill("black")
+text("GREGory",1151,270)
 }
 
 function mousePressed(){
-  let bullets = new bullet(609,168);
+  let bullets = new bullet(692,168);
   bulletsAR.push(bullets);
+flash = 15
 }
+
